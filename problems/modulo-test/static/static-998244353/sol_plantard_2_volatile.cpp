5d2af38f3679de71ee254548a9ab9446401847e5
hashiryo <hashimotoryoma@gmail.com>
2026-04-23 15:05:50 +0900

    asm volatile を入れてみる

--- problems/modulo-test/static/static-998244353/sol_plantard_2_volatile.cpp
+++ problems/modulo-test/static/static-998244353/sol_plantard_2_volatile.cpp
@@ -8,8 +8,8 @@ using u64= unsigned long long;
 using u128= __uint128_t;
 struct MP_Pl {  // mod < 2^32/phi
  u32 mod;
- constexpr MP_Pl(): mod(0), iv(0), r2(0) {}
- constexpr MP_Pl(u32 m): mod(m), iv(inv(m)), r2(-u128(mod) % mod) {}
+ MP_Pl(): mod(0), iv(0), r2(0) {}
+ MP_Pl(u32 m): mod(m), iv(inv(m)), r2(-u128(mod) % mod) { asm volatile("" : "+r"(mod), "+r"(iv), "+r"(r2)); }
  constexpr inline u32 mul(u32 l, u32 r) const { return reduce(u64(l) * r); }
  constexpr inline u32 set(u32 n) const { return mul(n, r2); }
  constexpr inline u32 get(u32 n) const { return reduce(n); }
