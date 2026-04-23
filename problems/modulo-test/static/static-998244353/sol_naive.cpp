0cff43ce9a786fea6aff33242025ae6db4f4a245
hashiryo <hashimotoryoma@gmail.com>
2026-04-23 18:02:00 +0900

    起動

--- problems/modulo-test/static/static-998244353/sol_naive.cpp
+++ problems/modulo-test/static/static-998244353/sol_naive.cpp
@@ -19,6 +19,7 @@ struct MP_Na {  // mod < 2^32
 signed main() {
  cin.tie(0);
  ios::sync_with_stdio(false);
+
  constexpr MP_Na mp(998244353);
  u32 n, state, a, b, c, d;
  cin >> n >> state >> a >> b >> c >> d;
