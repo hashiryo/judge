dc4cf1e1a68cdd422dae9c20f599575cf87837f8
hashiryo <hashimotoryoma@gmail.com>
2026-05-01 17:49:42 +0900

    実行

--- problems/yosupo-convolution-mod-1000000007/base.cpp
+++ problems/yosupo-convolution-mod-1000000007/base.cpp
@@ -6,34 +6,33 @@
 #define ALGO_HPP "algos/naive.hpp"
 #endif
 #include ALGO_HPP
-
 signed main() {
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    int N, M;
-    cin >> N >> M;
-    vector<u32> a(N), b(M);
-    for (auto& x : a) cin >> x;
-    for (auto& x : b) cin >> x;
+ cin.tie(0);
+ ios::sync_with_stdio(false);
+ int N, M;
+ cin >> N >> M;
+ vector<u32> a(N), b(M);
+ for(auto& x: a) cin >> x;
+ for(auto& x: b) cin >> x;
 
-    constexpr int REPEAT = 1;
-    uint64_t best_ns = ~uint64_t(0);
-    vector<u32> result;
+ constexpr int REPEAT= 1;
+ uint64_t best_ns= ~uint64_t(0);
+ vector<u32> result;
 
-    for (int rep = 0; rep < REPEAT; ++rep) {
-        auto t0 = chrono::steady_clock::now();
-        auto r = Conv::run(a, b);
-        auto t1 = chrono::steady_clock::now();
-        result = std::move(r);
-        auto ns = (uint64_t)chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count();
-        if (ns < best_ns) best_ns = ns;
-    }
+ for(int rep= 0; rep < REPEAT; ++rep) {
+  auto t0= chrono::steady_clock::now();
+  auto r= Conv::run(a, b);
+  auto t1= chrono::steady_clock::now();
+  result= std::move(r);
+  auto ns= (uint64_t)chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count();
+  if(ns < best_ns) best_ns= ns;
+ }
 
-    fprintf(stderr, "ALGO_TIME_NS=%llu\n", (unsigned long long)best_ns);
-    for (size_t i = 0; i < result.size(); ++i) {
-        if (i) cout << ' ';
-        cout << result[i];
-    }
-    cout << '\n';
-    return 0;
+ fprintf(stderr, "ALGO_TIME_NS=%llu\n", (unsigned long long)best_ns);
+ for(size_t i= 0; i < result.size(); ++i) {
+  if(i) cout << ' ';
+  cout << result[i];
+ }
+ cout << '\n';
+ return 0;
 }
