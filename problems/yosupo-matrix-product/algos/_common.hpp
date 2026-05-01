ad0313f47ee0c79145d635d04790b7ae95d7c66c
hashiryo <hashimotoryoma@gmail.com>
2026-05-01 19:16:48 +0900

    行列積 mod 998 追加

---
+++ problems/yosupo-matrix-product/algos/_common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+// algos 共通: typedef とよく使うヘッダ。
+// USE_SIMDE 環境では simde を先に include (float16_t 衝突回避)。
+#ifdef USE_SIMDE
+#include <simde/x86/avx2.h>
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using u8 = unsigned char;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+using u128 = __uint128_t;
+
+// この問題は mod 998244353 上の N×M × M×P の行列積。1 ≤ N, M, P ≤ 1024。
+constexpr u32 MOD = 998244353;
