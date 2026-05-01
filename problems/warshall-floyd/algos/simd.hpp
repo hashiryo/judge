aa440a8f9662ade4a3fb68ae6945ab58983913d0
hashiryo <hashimotoryoma@gmail.com>
2026-05-01 12:57:13 +0900

    起動

--- problems/warshall-floyd/algos/simd.hpp
+++ problems/warshall-floyd/algos/simd.hpp
@@ -13,12 +13,12 @@ struct WF {
  i32* d;
  WF(int v, int vp, i32* dist): V(v), Vp(vp), d(dist) {}
  void run() {
-  for (int k= 0; k < V; ++k) {
+  for(int k= 0; k < V; ++k) {
    const i32* dk= d + k * Vp;
-   for (int i= 0; i < V; ++i) {
+   for(int i= 0; i < V; ++i) {
     i32* di= d + i * Vp;
     __m256i vdik= _mm256_set1_epi32(di[k]);
-    for (int j= 0; j < Vp; j+= 8) {
+    for(int j= 0; j < Vp; j+= 8) {
      __m256i vdkj= _mm256_loadu_si256((const __m256i*)(dk + j));
      __m256i vdij= _mm256_loadu_si256((const __m256i*)(di + j));
      __m256i vsum= _mm256_add_epi32(vdik, vdkj);
