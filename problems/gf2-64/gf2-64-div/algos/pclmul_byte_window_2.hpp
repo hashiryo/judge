13545ea2867c46fb18aac6e9da5f076673a970bb
hashiryo <hashimotoryoma@gmail.com>
2026-05-05 17:33:59 +0900

    簡略化

--- problems/gf2-64/gf2-64-div/algos/pclmul_byte_window_2.hpp
+++ problems/gf2-64/gf2-64-div/algos/pclmul_byte_window_2.hpp
@@ -5,22 +5,17 @@
 #include "../../_shared/sq.hpp"
 #include "../../_shared/frob.hpp"
 namespace gf2_64_pclmul_itoh_tsujii {
+using gf2_64_pclmul::frob2;
 using gf2_64_pclmul::frob4;
 using gf2_64_pclmul::mul;
 using gf2_64_pclmul::sq;
 u64 inv(u64 a) {
- constexpr u64 e= 0xFFFFFFFFFFFFFFFEull;  // 2^64 - 2
  // Precompute T[i] = a^i for i = 0..15 (14 muls)
- u64 T[16]= {1, a};
- for(int i= 2; i < 16; ++i) T[i]= mul(T[i - 1], a);
-
- u64 acc= T[15];
- for(int i= 14; i >= 0; --i) {
-  acc= frob4(acc);
-  u16 chunk= (e >> (4 * i)) & 0xF;
-  acc= mul(acc, T[chunk]);
- }
- return acc;
+ u64 T14= sq(mul(mul(a, sq(a)), frob2(a)));
+ u64 T15= mul(T14, a);
+ u64 acc= frob4(T15);
+ for(int i= 14; i--;) acc= frob4(mul(acc, T15));
+ return mul(acc, T14);
 }
 }  // namespace gf2_64_pclmul_itoh_tsujii
 struct GF2_64Op {
