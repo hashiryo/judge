df0922bb13706a56414a397bb7e84e66dbe4b673
hashiryo <hashimotoryoma@gmail.com>
2026-05-05 17:55:09 +0900

    window サイズ変更

--- problems/gf2-64/gf2-64-div/algos/pclmul_byte_window_3.hpp
+++ problems/gf2-64/gf2-64-div/algos/pclmul_byte_window_3.hpp
@@ -6,16 +6,19 @@
 #include "../../_shared/frob.hpp"
 namespace gf2_64_pclmul_itoh_tsujii {
 using gf2_64_pclmul::frob2;
+using gf2_64_pclmul::frob3;
 using gf2_64_pclmul::frob4;
+using gf2_64_pclmul::frob8;
 using gf2_64_pclmul::mul;
 using gf2_64_pclmul::sq;
 u64 inv(u64 a) {
- // Precompute T[i] = a^i for i = 0..15 (14 muls)
- u64 T14= sq(mul(mul(a, sq(a)), frob2(a)));
- u64 T15= mul(T14, a);
- u64 acc= frob4(T15);
- for(int i= 14; i--;) acc= frob4(mul(acc, T15));
- return mul(acc, T14);
+ // 254
+ u64 TFE= sq(a);
+ for(int i= 6; i--;) TFE= sq(mul(a, TFE));
+ u64 TFF= mul(TFE, a);
+ u64 acc= frob8(TFF);
+ for(int i= 6; i--;) acc= frob8(mul(acc, TFF));
+ return mul(acc, TFE);
 }
 }  // namespace gf2_64_pclmul_itoh_tsujii
 struct GF2_64Op {
