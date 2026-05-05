5e225dd820ce5b441012a2542de3c39d775c091f
hashiryo <hashimotoryoma@gmail.com>
2026-05-05 16:23:43 +0900

    mul は速いのを使用

--- problems/gf2-64/gf2-64-div/algos/extgcd_2.hpp
+++ problems/gf2-64/gf2-64-div/algos/extgcd_2.hpp
@@ -14,13 +14,11 @@
 // 1 反復のコストは clz + シフト + XOR ≈ 数十 cycle 程度。
 #pragma GCC optimize("O3,unroll-loops")
 #include "../../_shared/_common.hpp"
-#include "../../_shared/sq.hpp"  // mul は使わないが既存 algo と同じヘッダ集合に揃える
+#include "../../_shared/mul.hpp"  // mul は使わないが既存 algo と同じヘッダ集合に揃える
+
+using gf2_64_pclmul::mul;
 namespace gf2_64_extgcd {
 using u128= __uint128_t;
-inline int clz128(u128 x) {
- u64 hi= u64(x >> 64);
- return hi ? __builtin_clzll(hi) : 64 + __builtin_clzll(u64(x));
-}
 constexpr u8 RED[]= {0, 27, 45, 54, 90, 65, 119, 108};
 // u128 (deg ≤ 127 多項式) を mod P (= x^64 + 0x1B) で u64 に還元。
 inline u64 reduce_modP(u128 x) {
@@ -30,12 +28,12 @@ inline u64 reduce_modP(u128 x) {
 inline u64 inv(u64 a) {
  if(!a) return 0;
 
- u128 s= 0, t= 1;
+ u64 s= 0, t= 1;
  u64 u= 0x1Bull ^ (a << (__builtin_clzll(a) + 1));  // P-a*x^shift
  s^= t << (__builtin_clzll(a) + 1);
  u64 v= a;
  while(v != 1) {
-  int du= 127 - clz128(u);
+  int du= 63 - __builtin_clzll(u);
   int dv= 63 - __builtin_clzll(v);
   if(du < dv) {
    u64 tmp= u;
@@ -54,23 +52,11 @@ inline u64 inv(u64 a) {
  }
  return reduce_modP(t);
 }
-inline u64 mul_naive(u64 a, u64 b) {
- // div = mul(a, inv(b)) のための単純 mul。本ファイル内で他の依存を増やさないために
- // bit-by-bit clmul + reduce で実装 (reference.hpp と同じ)。
- u64 lo= 0, hi= 0;
- for(int i= 0; i < 64; ++i) {
-  if((b >> i) & 1) {
-   lo^= a << i;
-   if(i) hi^= a >> (64 - i);
-  }
- }
- return reduce_modP((u128)hi << 64 | lo);
-}
 }  // namespace gf2_64_extgcd
 struct GF2_64Op {
  static vector<u64> run(const vector<u64>& as, const vector<u64>& bs) {
   vector<u64> ans(as.size());
-  for(size_t i= 0; i < as.size(); ++i) ans[i]= gf2_64_extgcd::mul_naive(as[i], gf2_64_extgcd::inv(bs[i]));
+  for(size_t i= 0; i < as.size(); ++i) ans[i]= mul(as[i], gf2_64_extgcd::inv(bs[i]));
   return ans;
  }
 };
