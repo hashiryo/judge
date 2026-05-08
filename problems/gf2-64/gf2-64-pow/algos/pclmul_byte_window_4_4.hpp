aa1f325148fd000774ff4044f8978ad4979ea1fe
hashiryo <hashimotoryoma@gmail.com>
2026-05-08 17:56:35 +0900

    256 を使い回す

--- problems/gf2-64/gf2-64-pow/algos/pclmul_byte_window_4_4.hpp
+++ problems/gf2-64/gf2-64-pow/algos/pclmul_byte_window_4_4.hpp
@@ -28,18 +28,19 @@ namespace gf2_64_pow_byte_window_vpclmul {
 using gf2_64_pclmul::frob4;
 using gf2_64_pclmul::mul;
 using gf2_64_pclmul::sq;
-constexpr u8 RED[]= {0, 27, 45, 54, 90, 65, 119, 108};
-// 2 並列 mul: (mul(a0, b0), mul(a1, b1)) を VPCLMULQDQ + 並列 reduction で計算
-VPCLMUL inline void mul2_(const __m256i& a_vec, const __m256i& b_vec, u64& r0, u64& r1) {
+const __m256i RED_TABLE= _mm256_setr_epi8(0, 27, 45, 54, 90, 65, 119, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 27, 45, 54, 90, 65, 119, 108, 0, 0, 0, 0, 0, 0, 0, 0);
+VPCLMUL inline __m256i mul2(const __m256i& a_vec, const __m256i& b_vec, u64& r0, u64& r1) {
  __m256i prod= _mm256_clmulepi64_epi128(a_vec, b_vec, 0);
  __m256i d_full= _mm256_xor_si256(prod, _mm256_slli_epi64(prod, 1));
  __m256i red1_full= _mm256_xor_si256(d_full, _mm256_slli_epi64(d_full, 3));
  __m256i red1_shift= _mm256_srli_si256(red1_full, 8);
- __m256i partial= _mm256_xor_si256(prod, red1_shift);
- __m128i p0= _mm256_castsi256_si128(partial);
- __m128i p1= _mm256_extracti128_si256(partial, 1);
- r0= u64(p0[0]) ^ RED[p0[1] >> 60];
- r1= u64(p1[0]) ^ RED[p1[1] >> 60];
+ __m256i h_idx= _mm256_srli_epi64(prod, 60);
+ __m256i indices= _mm256_srli_si256(h_idx, 8);
+ __m256i red_vec= _mm256_shuffle_epi8(RED_TABLE, indices);
+ __m256i result= _mm256_xor_si256(_mm256_xor_si256(prod, red1_shift), red_vec);
+ r0= _mm256_castsi256_si128(result)[0];
+ r1= _mm256_extracti128_si256(result, 1)[0];
+ return result;
 }
 u64 pow(u64 a, u64 e) {
  if(e == 0) return 1;
@@ -47,17 +48,16 @@ u64 pow(u64 a, u64 e) {
  u64 T[16]= {1, a, sq(a)};
  // L2: T[3], T[4]
  __m256i T2a= _mm256_set_epi64x(0, T[2], 0, a);
- mul2_(_mm256_set1_epi64x(T[2]), T2a, T[3], T[4]);
+ __m256i T43= mul2(T2a, _mm256_set1_epi64x(T[2]), T[3], T[4]);
  // L3: T[5..8]
  __m256i T4= _mm256_set1_epi64x(T[4]);
- __m256i T43= _mm256_set_epi64x(0, T[4], 0, T[3]);
- mul2_(T4, T2a, T[5], T[6]);
- mul2_(T4, T43, T[7], T[8]);
+ __m256i T56= mul2(T4, T2a, T[5], T[6]);
+ mul2(T4, T43, T[7], T[8]);
  // L4: T[9..15] (T[15] は単独 mul)
  __m256i T8= _mm256_set1_epi64x(T[8]);
- mul2_(T8, T2a, T[9], T[10]);
- mul2_(T8, T43, T[11], T[12]);
- mul2_(T8, _mm256_set_epi64x(0, T[6], 0, T[5]), T[13], T[14]);
+ mul2(T8, T2a, T[9], T[10]);
+ mul2(T8, T43, T[11], T[12]);
+ mul2(T8, T56, T[13], T[14]);
  T[15]= mul(T[8], T[7]);
  // メイン loop: 4-bit nibble ごとに frob4 + mul で進める (serial chain なので変更なし)
  int top= 15 - (__builtin_clzll(e) >> 2);
